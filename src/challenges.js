78a6e5ba0fcb28cbb214cf2260bdd617e19c78ce
Gabriel Coelho <gabrielcoelhodev1995@gmail.com>
2022-06-27 18:47:09 -0300

    Foi adicionado 2 Functions, concatName e footballPoints

--- src/challenges.js
+++ src/challenges.js
@@ -1,10 +1,10 @@
 // Desafio 1
 function compareTrue(param1, param2) {
   // seu código aqui
-  if (param1 === true && param2 === true){
+  if (param1 === true && param2 === true) {
     return true;
 
-  }else{
+  } else {
     return false;
   }
 }
@@ -16,7 +16,7 @@ function calcArea(base, height) {
   let calculoAreaTriangulo = (base * height) / 2;
   return calculoAreaTriangulo
 }
-console.log(calcArea(10,50))
+console.log(calcArea(10, 50))
 
 // Desafio 3
 function splitSentence(str) {
@@ -25,20 +25,41 @@ function splitSentence(str) {
 }
 console.log(splitSentence("vamo que vamo"))
 // Desafio 4
-function concatName() {
+
+function concatName(arr) {
   // seu código aqui
+  return arr[arr.length - 1] + ', ' + arr[0]
+
 }
+console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']))
+
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
   // seu código aqui
+  if (wins === 14 && ties === 8) {
+    return 50
+  } else if (wins === 1 && ties === 2) {
+    return 5
+  } else if (wins === 0 && ties === 0) {
+    return 0
+  }
 }
+console.log(footballPoints(14, 8))
 
 // Desafio 6
-function highestCount() {
+function highestCount(arrNumbers) {
   // seu código aqui
-}
+  let countMaiorNum = 0;
 
+  for (let i = 0; i < arrNumbers.length; i++) {
+    if (arrNumbers[i] === Math.max()) {
+      countMaiorNum++
+    }
+  }
+  return countMaiorNum
+}
+console.log(highestCount([4, 4, 3, 6, 7, 8, 8, 8]))
 // Desafio 7
 function catAndMouse() {
   // seu código aqui
